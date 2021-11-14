b2c6875a59f2d4ca3cfadddf0fbeffaef7ab688d
HaXapakTepe <haxapaktepe@gmail.com>
2021-11-14 23:55:51 +0400

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -108,13 +108,10 @@ $(function () {
     $('.language__link').toggleClass('language__link--active');
   });
 
-  // $('.faq__subtitle').on('click', function () {
-  //   $('.faq__text').toggleClass('faq__text--active');
-  // });
-
   $('.faq__subtitle').on('click', function () {
-    $('.faq__arrow').toggleClass('faq__arrow--active');
+    $('.faq__text').toggleClass('faq__text--active');
   });
+
 
   $('.popup__open').click(function (e) {
     e.preventDefault();
